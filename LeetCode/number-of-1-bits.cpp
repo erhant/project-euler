bae8564962e1f4bae1c766f15d387d6a3970f2ff
Erhan Tezcan <erhant96@hotmail.com>
2020-10-26 22:52:23 +0300

    hamming weight

---
+++ LeetCode/number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int hammingWeight(uint32_t n) {
+    int sum = 0, i = 0;
+    for (; i<32 && n > 0; i++) {
+      sum += !((n & 1) == 0);
+      n = n >> 1;        
+    }
+    return sum;
+  }
+  
+};
