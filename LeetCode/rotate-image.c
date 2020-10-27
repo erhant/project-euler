470b1e42f9d4dea231150a5250c4df2af2e6ce99
Erhan Tezcan <erhant96@hotmail.com>
2020-10-27 16:16:28 +0300

    rotate image

--- LeetCode/rotate-image.c
+++ LeetCode/rotate-image.c
@@ -1,14 +1,19 @@
+/*
+right = matrixSize - 1 - i;
+left = i;
+up = i;
+down = matrixSize - 1 - i;
+*/
 void rotate(int** matrix, int matrixSize, int* matrixColSize){
-  int tmp;
-  int n = matrixSize - 1;
+  int tmp, a, b, c; // a b c to optimize
   for (int i = 0; i < matrixSize / 2; i++) {
-    for (int j = i; j < n - 1 - i; j++) {
-      tmp = matrix[i][j];
-      matrix[i][j] = matrix[n-i-j][i];
-      matrix[n-i-j][i] = matrix[n-i][n-i-j];
-      matrix[n-i][n-i-j] = matrix[j][n-i];
-      matrix[j][n-i] = tmp;
+    for (int j = 0; j < matrixSize - 1 - 2*i; j++) {
+      tmp = matrix[i][i + j];
+      matrix[i][i + j] = matrix[matrixSize - 1 - i - j][i];
+      matrix[matrixSize - 1 - i - j][i] = matrix[matrixSize - 1 - i][matrixSize - 1 - i - j];
+      matrix[matrixSize - 1 - i][matrixSize - 1 - i - j] = matrix[i + j][matrixSize - 1 - i];
+      matrix[i + j][matrixSize - 1 - i] = tmp;
     }
   }
-  // todo: not working
 }
+// O(N^2) time, O(1) space
