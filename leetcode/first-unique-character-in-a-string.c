f9c66a36ac3106f0584fd614caf7d7e50e1969bf
Erhan Tezcan <erhany96@gmail.com>
2021-07-27 22:13:49 +0300

    first char

---
+++ leetcode/first-unique-character-in-a-string.c
@@ -0,0 +1,13 @@
+
+int firstUniqChar(char* s) {
+  int COUNT[26] = {0}, i;
+  for (i = 0; s[i] && ++COUNT[s[i] - 'a']; i++)
+    ;
+  for (i = 0; s[i] && COUNT[s[i] - 'a'] != 1; i++)
+    ;
+  return s[i] == '\0' ? -1 : i;
+}
+
+/*
+Deliberately ugly :)
+*/
