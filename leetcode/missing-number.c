f5784e7b89e25a50e41afa24c7cb7db423363d9a
Erhan Tezcan <erhany96@gmail.com>
2022-02-01 22:47:21 +0300

    mn

--- leetcode/missing-number.c
+++ leetcode/missing-number.c
@@ -9,22 +9,21 @@ difficulty: easy
 int missingNumber(int* nums, int n) {
   int i, src;
 
-  // paint the indexed numbers negative
-  // +1 because 0 can't be painted
-  for (int i = 0; i < n; ++i) {
+  // paint the indexed numbers negative, add 1 to everything to handle 0
+  for (i = 0; i < n; ++i) {
     src = nums[i];                     // source index is nums[i]
     src = src < 0 ? -(src + 1) : src;  // this may be painted before, so neutralize it
+
     // paint the destination
     if (src != n) {
       nums[src] = -(nums[src] + 1);
     }
   }
 
-  // find the first non-painted value
-  for (int i = 0; i < n; ++i) {
-    if (nums[i] >= 0) {
-      return i;
-    }
-  }
-  return n;
+  // loop until the end or first non-negative
+  for (i = 0; i < n && nums[i] < 0; ++i)
+    ;
+
+  // return it's index
+  return i;
 }
